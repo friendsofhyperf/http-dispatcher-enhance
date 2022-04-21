0ede4a3209042500ad053f0e408ce30f99093c5f
D.J.Hwang <huangdijia@gmail.com>
2022-04-21 08:51:20 +0800

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of friendsofhyperf/http-dispatcher-enhance.
  *
  * @link     https://github.com/friendsofhyperf/http-dispatcher-enhance
- * @document https://github.com/friendsofhyperf/http-dispatcher-enhance/blob/1./README.md
+ * @document https://github.com/friendsofhyperf/http-dispatcher-enhance/blob/1.x/README.md
  * @contact  huangdijia@gmail.com
  */
 $header = <<<'EOF'
